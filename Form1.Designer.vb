af7a6569c26c30bda0af18e56a40c43d8f1ffa06
ezekieljames60 <137297922+ezekieljames60@users.noreply.github.com>
2023-06-24 10:27:27 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,381 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        TextBox1 = New TextBox()
+        Button17 = New Button()
+        PrintDocument1 = New Printing.PrintDocument()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        ButtonSin = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button1.Font = New Font("Arial Rounded MT Bold", 7.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.ForeColor = Color.Blue
+        Button1.Location = New Point(216, 281)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button2.ForeColor = Color.Blue
+        Button2.Location = New Point(328, 281)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button3.ForeColor = Color.Blue
+        Button3.Location = New Point(451, 281)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 2
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.Blue
+        Button4.ForeColor = Color.Yellow
+        Button4.Location = New Point(568, 281)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 3
+        Button4.Text = "+"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button5.ForeColor = Color.Blue
+        Button5.Location = New Point(216, 316)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(94, 29)
+        Button5.TabIndex = 4
+        Button5.Text = "4"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button6.ForeColor = Color.Blue
+        Button6.Location = New Point(328, 316)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(94, 29)
+        Button6.TabIndex = 5
+        Button6.Text = "5"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button7.ForeColor = Color.Blue
+        Button7.Location = New Point(451, 316)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(94, 29)
+        Button7.TabIndex = 6
+        Button7.Text = "6"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.Blue
+        Button8.ForeColor = Color.Yellow
+        Button8.Location = New Point(567, 316)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(94, 29)
+        Button8.TabIndex = 7
+        Button8.Text = "-"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button9.ForeColor = Color.Blue
+        Button9.Location = New Point(216, 351)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(94, 29)
+        Button9.TabIndex = 8
+        Button9.Text = "7"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button10.ForeColor = Color.Blue
+        Button10.Location = New Point(328, 351)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(94, 29)
+        Button10.TabIndex = 9
+        Button10.Text = "8"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button11.ForeColor = Color.Blue
+        Button11.Location = New Point(451, 351)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(94, 29)
+        Button11.TabIndex = 10
+        Button11.Text = "9"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.Blue
+        Button12.ForeColor = Color.Yellow
+        Button12.Location = New Point(567, 351)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(94, 29)
+        Button12.TabIndex = 11
+        Button12.Text = "*"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button13.ForeColor = Color.Blue
+        Button13.Location = New Point(216, 386)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(94, 29)
+        Button13.TabIndex = 12
+        Button13.Text = "0"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(255))
+        Button14.Cursor = Cursors.Hand
+        Button14.ForeColor = Color.Blue
+        Button14.Location = New Point(328, 386)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(94, 29)
+        Button14.TabIndex = 13
+        Button14.Text = "."
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.Blue
+        Button15.ForeColor = Color.Yellow
+        Button15.Location = New Point(451, 386)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(94, 29)
+        Button15.TabIndex = 14
+        Button15.Text = "C"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.Blue
+        Button16.ForeColor = Color.Yellow
+        Button16.Location = New Point(567, 386)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(94, 29)
+        Button16.TabIndex = 15
+        Button16.Text = "/"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.Cyan
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Font = New Font("Microsoft Sans Serif", 28.2F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox1.Location = New Point(216, 214)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(445, 61)
+        TextBox1.TabIndex = 16
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.Lime
+        Button17.ForeColor = Color.Red
+        Button17.Location = New Point(567, 421)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(94, 29)
+        Button17.TabIndex = 17
+        Button17.Text = "="
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.KERAMA1
+        PictureBox1.Location = New Point(2, -2)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(669, 148)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 18
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Yellow
+        Label1.ForeColor = Color.Blue
+        Label1.Location = New Point(216, 163)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(277, 20)
+        Label1.TabIndex = 19
+        Label1.Text = "THE INVENTOR:KERAMA EZEKIEL JAMES"
+        ' 
+        ' ButtonSin
+        ' 
+        ButtonSin.BackColor = Color.Lime
+        ButtonSin.ForeColor = Color.FromArgb(CByte(64), CByte(0), CByte(64))
+        ButtonSin.Location = New Point(216, 421)
+        ButtonSin.Name = "ButtonSin"
+        ButtonSin.Size = New Size(94, 29)
+        ButtonSin.TabIndex = 20
+        ButtonSin.Text = "sin"
+        ButtonSin.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Button19.ForeColor = Color.Blue
+        Button19.Location = New Point(328, 421)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(94, 29)
+        Button19.TabIndex = 21
+        Button19.Text = "cos"
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.ForeColor = Color.Black
+        Button20.Location = New Point(451, 421)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(94, 29)
+        Button20.TabIndex = 22
+        Button20.Text = "Tan"
+        Button20.UseVisualStyleBackColor = True
+        ' 
+        ' Button21
+        ' 
+        Button21.BackColor = Color.Black
+        Button21.ForeColor = Color.Fuchsia
+        Button21.Location = New Point(216, 456)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(94, 29)
+        Button21.TabIndex = 23
+        Button21.Text = "srt"
+        Button21.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Red
+        ClientSize = New Size(674, 569)
+        Controls.Add(Button21)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(ButtonSin)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(Button17)
+        Controls.Add(TextBox1)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        ForeColor = Color.Red
+        Name = "Form1"
+        Text = "SCIENTIFIC CALCULATOR"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button17 As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonSin As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+End Class
