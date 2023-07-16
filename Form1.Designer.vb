53ed4ea3cf6621d1e97add4e14a0bc7aa078813e
ezekieljames60 <137297922+ezekieljames60@users.noreply.github.com>
2023-07-16 06:20:55 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnLOGIN = New Button()
+        btnEXIT = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnLOGIN
+        ' 
+        btnLOGIN.BackColor = Color.Green
+        btnLOGIN.Location = New Point(390, 416)
+        btnLOGIN.Margin = New Padding(4, 3, 4, 3)
+        btnLOGIN.Name = "btnLOGIN"
+        btnLOGIN.Size = New Size(118, 33)
+        btnLOGIN.TabIndex = 0
+        btnLOGIN.Text = "LOGIN"
+        btnLOGIN.UseVisualStyleBackColor = False
+        ' 
+        ' btnEXIT
+        ' 
+        btnEXIT.BackColor = Color.Red
+        btnEXIT.Location = New Point(546, 416)
+        btnEXIT.Margin = New Padding(4, 3, 4, 3)
+        btnEXIT.Name = "btnEXIT"
+        btnEXIT.Size = New Size(118, 33)
+        btnEXIT.TabIndex = 1
+        btnEXIT.Text = "EXIT"
+        btnEXIT.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(289, 299)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 23)
+        Label1.TabIndex = 2
+        Label1.Text = "Username"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(293, 360)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(85, 23)
+        Label2.TabIndex = 3
+        Label2.Text = "Password"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(192))
+        TextBox1.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        TextBox1.Location = New Point(390, 296)
+        TextBox1.Margin = New Padding(4, 3, 4, 3)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(274, 30)
+        TextBox1.TabIndex = 4
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        TextBox2.ForeColor = Color.White
+        TextBox2.Location = New Point(390, 357)
+        TextBox2.Margin = New Padding(4, 3, 4, 3)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PasswordChar = "*"c
+        TextBox2.Size = New Size(274, 30)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Blue
+        Label3.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(330, -1)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(378, 41)
+        Label3.TabIndex = 6
+        Label3.Text = "STUDENT LOGIN SYSTEM"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Lime
+        Label4.Location = New Point(434, 40)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(149, 23)
+        Label4.TabIndex = 7
+        Label4.Text = "STUDENT PHOTO"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(390, 66)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(274, 199)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 8
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Purple
+        ClientSize = New Size(1132, 659)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnEXIT)
+        Controls.Add(btnLOGIN)
+        Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        ForeColor = Color.Yellow
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "LOGINSYSTEM"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnLOGIN As Button
+    Friend WithEvents btnEXIT As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
--- Form1.vb
+++ Form1.vb
@@ -1,177 +1,21 @@
 ﻿Public Class Form1
-    Dim firstnum As Decimal
-    Dim secondnum As Decimal
-    Dim operations As Integer
-    Dim operator_selector As Boolean = False
-    Dim pie = 3.142
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "1"
+    Private Sub btnLOGIN_Click(sender As Object, e As EventArgs) Handles btnLOGIN.Click
+        If TextBox1.Text = "james" And TextBox2.Text = "ezekiel" Then
+            MessageBox.Show("Login successful")
         Else
-            TextBox1.Text = "1"
-
+            MessageBox.Show("Invalid username or password,please check and try again later")
         End If
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "2"
-        Else
-            TextBox1.Text = "2"
-
-        End If
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "3"
-        Else
-            TextBox1.Text = "3"
-
-        End If
-    End Sub
-
-    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "4"
-        Else
-            TextBox1.Text = "4"
-
-        End If
-    End Sub
-
-    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "5"
-        Else
-            TextBox1.Text = "5"
-
-        End If
-    End Sub
-
-    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "6"
-        Else
-            TextBox1.Text = "6"
-
-        End If
-    End Sub
-
-    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "7"
-        Else
-            TextBox1.Text = "7"
-
-        End If
-    End Sub
-
-    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "8"
-        Else
-            TextBox1.Text = "8"
-
-        End If
-    End Sub
-
-    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "9"
-        Else
-            TextBox1.Text = "9"
-
-        End If
-    End Sub
-
-    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
-        If TextBox1.Text <> "0" Then
-            TextBox1.Text += "0"
-
-
-        End If
-    End Sub
-
-    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
-        If Not (TextBox1.Text.Contains(".")) Then
-            TextBox1.Text += "."
-        End If
-    End Sub
-
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        firstnum = TextBox1.Text
-        TextBox1.Text = "0"
-
-        operator_selector = True
-        operations = 1 '= +
-
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
 
     End Sub
 
-    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
-        firstnum = TextBox1.Text
-        TextBox1.Text = "0"
-        operator_selector = True
-        operations = 2 '= -
-    End Sub
-
-    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
-        firstnum = TextBox1.Text
-        TextBox1.Text = "0"
-
-        operator_selector = True
-        operations = 3 '= *
-    End Sub
-
-    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
-        firstnum = TextBox1.Text
-        TextBox1.Text = "0"
-
-        operator_selector = True
-        operations = 4 '= /
-    End Sub
-
-    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
-        If operator_selector = True Then
-            secondnum = TextBox1.Text
-            If operations = 1 Then
-                TextBox1.Text = firstnum + secondnum
-            ElseIf operations = 2 Then
-                TextBox1.Text = firstnum + secondnum
-            ElseIf operations = 3 Then
-                TextBox1.Text = firstnum * secondnum
-            Else
-                If secondnum = 0 Then
-                    TextBox1.Text = "Please Mathematician the data entered is invalid,try again later!"
-                Else
-                    TextBox1.Text = firstnum / secondnum
-
-                End If
-            End If
-            operator_selector = False
-        End If
-    End Sub
-
-    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
-        TextBox1.Text = "0"
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
-
-    Private Sub ButtonSin_Click(sender As Object, e As EventArgs) Handles ButtonSin.Click
-        Dim value As Double
-        If Double.TryParse(TextBox1.Text, value) Then
-            Dim result As Double = Math.Sin(value)
-            TextBox1.Text = result.ToString()
-        Else
-            MessageBox.Show("Invalid input. Please enter a valid number.")
+    Private Sub btnEXIT_Click(sender As Object, e As EventArgs) Handles btnEXIT.Click
+        Dim i As DialogResult
+        i = MessageBox.Show("Confirm if you want to exit", "LOGINSYSTEM", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If i = DialogResult.Yes Then
+            Application.Exit()
         End If
     End Sub
 End Class
